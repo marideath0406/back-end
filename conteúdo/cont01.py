267f485f9d87133b6b393309110a0fc3bc7e632d
marideath0406 <maria.bueno.sousa@escola.pr.gov.br>
2026-03-05 11:46:05 -0300

    Add comments explaining data types in cont01.py

---
+++ conteúdo/cont01.py
@@ -0,0 +1,4 @@
+#int= para numeros inteiros. Ex: 7, 0, -4
+#float= para numeros reais ou com virgula. Ex: 0.45, 7.6
+#bool= para valores lógicos de verdadeiro ou falso. Ex True ou False( sempre usar com letra maiúscula
+#str= para strings que são palavras caracteres ou textos. Ex: 'ola', '7.5'
