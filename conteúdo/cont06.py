43b86f4ba765b9cdc7bfd6917b2067f04e737e23
marideath0406 <maria.bueno.sousa@escola.pr.gov.br>
2026-03-16 10:55:49 -0300

    Update cont06.py

--- conteúdo/cont06.py
+++ conteúdo/cont06.py
@@ -8,3 +8,19 @@ print('prazer em conhece-lo {:20}!'.format(nome))
 Qual é o seu nome?Gustavo
 
 prazer em conhece-lo Gustavo
+
+ALINHAMENTO DE MÁSCARA
+
+#Podemos alinhar o nome dentro do número de caracteres que desejamos
+
+#Alinhamento para direita
+nome input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:>20}11.format(nome))
+
+#Alinhamento para a esquerda
+nome input ('Qual é o seu nome?')
+print('prazer em conhece-lo (:<20)!'.format(nome))
+
+#Alinhamento no centro
+nome input ('Qual é o seu nome?')
+print('prazer em conhece-10 {:^20)!'.format(nome))
