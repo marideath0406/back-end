bedb789c3629ac7881e6f4671b9ad1b91fe6b27f
marideath0406 <maria.bueno.sousa@escola.pr.gov.br>
2026-03-16 10:48:28 -0300

    Create cont06.py for user greeting
    Add a script to greet the user with formatted name output.

---
+++ conteúdo/cont06.py
@@ -0,0 +1,10 @@
+OPERADORES
+#Podemos escrever um resultado na quantidade de caracteres específica ao colocar o valor dentro da mácara após escrever
+
+nome = input ('Qual é o seu nome?')
+
+print('prazer em conhece-lo {:20}!'.format(nome))
+
+Qual é o seu nome?Gustavo
+
+prazer em conhece-lo Gustavo
