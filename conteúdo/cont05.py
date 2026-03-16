9148726a8e16d6e5870e931d6168d7521e2fdad9
marideath0406 <maria.bueno.sousa@escola.pr.gov.br>
2026-03-16 10:43:30 -0300

    Create cont05.py with operator precedence details
    Add content explaining operator precedence and examples in Python.

---
+++ conteúdo/cont05.py
@@ -0,0 +1,27 @@
+ORDEM DE PRECEDÊNCIA
+#Definição: é a ordem na qual os operadores serão executados
+
+#1 Expressões dentro de parenteses
+
+#2 Em seguida vem as potenciações
+
+#3 Em terceiro lugar vem a multiplicação, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais de um deles você resolve quem aparecer primeiro
+
+#4 E por último a soma e a subtração
+
+EXEMPLO
+#5+3*2==
+
+#3^ * 5 + 4 ^ 2 =
+
+#3^ * (5 + 4) ^ 2 =
+
+#O Python é utilizado em cálculos científicos é o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
+
+#Para exponenciação podemos usar o pow(x,y) onde o número x é elevado aou número 
+
+#Para raiz quadrada podemos usar o **(1/2), elevando o número a 1/2 nos obtemos a raiz quadrada, para raiz cúbica **(1/3), e assim por diante.
+
+#Podemos usar os operadores em strings
+
+#Ex '='*5 vai ficar '====='
