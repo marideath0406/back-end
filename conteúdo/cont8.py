f9099ebaeaea2ccf45c7bad8ecb9f5659549d7b1
marideath0406 <maria.bueno.sousa@escola.pr.gov.br>
2026-03-26 09:01:55 -0300

    Create cont8.py

---
+++ conteúdo/cont8.py
@@ -0,0 +1,7 @@
+importando funcionalidades específicas
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) arredonda pra cima
+print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
+
