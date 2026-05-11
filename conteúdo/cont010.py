c1e3ef30cd4f6566742c8ff70794c96167dc99a3
marideath0406 <maria.bueno.sousa@escola.pr.gov.br>
2026-05-11 10:53:01 -0300

    Update and rename ex010.py to cont010.py

---
+++ conteúdo/cont010.py
@@ -0,0 +1,21 @@
+Condições
+Em condicionais devemos deixar o código indentado, ou seja,
+com o espaçamento correto demonstrando qual bloco de código pertece ao operador condicional. 
+Para realizar a indentração podemos utilizar a tecla tab. Em python para o se utilizamos if+confição+:,
+já o senão utilizamos o else + :, depois dos dois pontos nos dois casos colocamos os blocos de códigos que serão executados.
+Nesse tipo de condicional apenas um bloco é executado.
+
+Em condicionais devemos deixar o código indentado, ou seja, com o espaçamento correto demonstrando qual 
+bloco de código pertece ao operador condicional.
+Para realizar a indentração podemos utilizar a tecla tab. Em python para o se utilizamos if+confição+:,
+já o senão utilizamos o else + :, depois dos dois pontos nos dois casos colocamos os blocos de códigos que serão executados.
+Nesse tipo de condicional apenas um bloco é executado.
+
+
+  Condições (exemplo)
+tempo = int(input("Quantos anos tem seu carro?"))
+If tempo<=3:
+print('carro novo')
+else:
+print('carro velho)
+Print('--fim--')
--- conteúdo/ex010.py
+++
@@ -1,6 +0,0 @@
-Condições
-Em condicionais devemos deixar o código indentado, ou seja,
-com o espaçamento correto demonstrando qual bloco de código pertece ao operador condicional. 
-Para realizar a indentração podemos utilizar a tecla tab. Em python para o se utilizamos if+confição+:,
-já o senão utilizamos o else + :, depois dos dois pontos nos dois casos colocamos os blocos de códigos que serão executados.
-Nesse tipo de condicional apenas um bloco é executado.
